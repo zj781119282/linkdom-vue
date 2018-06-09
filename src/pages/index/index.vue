063ab45779b62c0c2a982fdd9b83f8da2e071d5d
zj781119282 <zj781119282@163.com>
2018-06-09 22:37:12 +0800

    update index page

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,18 +3,42 @@
     <head-top page="index"/>
     <div class="content">
       <banner/>
-      <section class="index-section index-cryption">
-        <div class="block-container clearfix">
-          <article class="text-right-lg text-center-xs">
-            <h2>{{$t('INDEX.CRYPTION_TITLE1')}}</h2>
-            <h2>{{$t('INDEX.CRYPTION_TITLE2')}}</h2>
-          </article>
-          <div class="image-container">
-            <img src="./assets/index-cryption.png" alt=""/>
-          </div>
-        </div>
+      <!--<section class="index-section index-cryption">-->
+        <!--<div class="block-container clearfix">-->
+          <!--<article class="text-right-lg text-center-xs">-->
+            <!--<h2>{{$t('INDEX.CRYPTION_TITLE1')}}</h2>-->
+            <!--<h2>{{$t('INDEX.CRYPTION_TITLE2')}}</h2>-->
+          <!--</article>-->
+          <!--<div class="image-container">-->
+            <!--<img src="./assets/index-cryption.png" alt=""/>-->
+          <!--</div>-->
+        <!--</div>-->
+      <!--</section>-->
+      <!--<section class="index-section cold-hot"></section>-->
+
+      <section class="prod">
+        <img src="./assets/prod.png" alt="LinkDam"/>
       </section>
-      <section class="index-section cold-hot"></section>
+
+      <section class="prod">
+        <img src="./assets/prod-2.png" alt="LinkDam"/>
+      </section>
+
+      <section class="prod">
+        <img src="./assets/prod-3.png" alt="LinkDam"/>
+      </section>
+
+      <section class="prod">
+        <img src="./assets/prod-4.png" alt="LinkDam"/>
+      </section>
+
+      <section class="prod">
+        <img src="./assets/prod-5.png" alt="LinkDam"/>
+      </section>
+
+      <section class="prod">
+        <img src="./assets/prod-6.png" alt="LinkDam"/>
+      </section>
       <foot/>
     </div>
     <loading v-show="!loaded"></loading>
